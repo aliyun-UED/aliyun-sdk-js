1ad571c837e748feefedae849afb29bf700fa013
xiongliang.xl <xiongliang.xl@alibaba-inc.com>
2015-01-22 15:06:33 +0800

    update samples

--- samples/oss/CopyObject.js
+++ samples/oss/CopyObject.js
@@ -13,14 +13,14 @@ oss.copyObject({
     //CopySourceIfModifiedSince: '',
     //CopySourceIfUnmodifiedSince: '',
     //CopySourceIfModifiedSince: '',
-    //MetadataDirective: '',
+    MetadataDirective: 'REPLACE',     // 'REPLACE' 表示覆盖 meta 信息，'COPY' 表示不覆盖，只拷贝
     //AccessControlAllowOrigin: '',
     ContentType: 'text/plain',
     CacheControl: 'no-cache',         // 参考: http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.9
     ContentDisposition: '',           // 参考: http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.5.1
     ContentEncoding: 'utf-8',         // 参考: http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.11
     ServerSideEncryption: 'AES256',
-    Expires: 60
+    Expires: ''                       // 参考: http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.21
   },
   function (err, data) {
 
