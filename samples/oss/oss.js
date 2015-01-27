ecf7ef7febd525249c1a85bb88149bac6be4cb9f
xiongliang.xl <xiongliang.xl@alibaba-inc.com>
2015-01-27 11:26:40 +0800

    update sample

--- samples/oss/oss.js
+++ samples/oss/oss.js
@@ -9,6 +9,12 @@ var oss = new ALY.OSS({
   // 青岛：http://oss-cn-qingdao.aliyuncs.com
   // 深圳：http://oss-cn-shenzhen.aliyuncs.com
   // 香港：http://oss-cn-hongkong.aliyuncs.com
+  // 注意：如果你是在 ECS 上连接 OSS，可以使用内网地址，速度快，没有带宽限制。
+  // 杭州：http://oss-cn-hangzhou.aliyuncs.com
+  // 北京：http://oss-cn-beijing.aliyuncs.com
+  // 青岛：http://oss-cn-qingdao.aliyuncs.com
+  // 深圳：http://oss-cn-shenzhen.aliyuncs.com
+  // 香港：http://oss-cn-hongkong.aliyuncs.com
   endpoint: 'http://oss-cn-hangzhou.aliyuncs.com',
   // 这是 oss sdk 目前支持最新的 api 版本, 不需要修改
   apiVersion: '2013-10-15'
