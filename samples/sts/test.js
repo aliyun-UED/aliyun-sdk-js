6995db345b31009abf2f4261400942a2a928fbd5
xiongliang.xl <xiongliang.xl@alibaba-inc.com>
2016-01-28 11:37:51 +0800

    update sts sample

--- samples/sts/test.js
+++ samples/sts/test.js
@@ -8,12 +8,16 @@ var sts = new ALY.STS({
     }
 );
 
-sts.getFederationToken({
-  StsVersion: '1',
-  Action: 'GetFederationToken',
-  Name: 'username',
-  Policy: '{"Version":"1","Statement":[{"Effect":"Allow", "Action":"*", "Resource":"*"}]}',
-  DurationSeconds: 1000
+// 构造AssumeRole请求
+sts.assumeRole({
+  Action: 'AssumeRole',
+  // 指定角色Arn
+  RoleArn: '<role-arn>',
+  //设置Token的附加Policy，可以在获取Token时，通过额外设置一个Policy进一步减小Token的权限；
+  //Policy: '{"Version":"1","Statement":[{"Effect":"Allow", "Action":"*", "Resource":"*"}]}',
+  //设置Token有效期，可选参数，默认3600秒；
+  //DurationSeconds: 3600,
+  RoleSessionName: 'RoleSessionName'
 }, function (err, res) {
   console.log(err, res);
 });
