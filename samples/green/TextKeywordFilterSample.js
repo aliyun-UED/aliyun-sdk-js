9d36bdd83b904d88f99b423036436dc0e49cde97
liuhai.lh <liuhai.lh@alibaba-inc.com>
2016-12-05 10:10:14 +0800

    green-sdk_v20161124
    green-sdk_v20161124

---
+++ samples/green/TextKeywordFilterSample.js
@@ -0,0 +1,27 @@
+/**
+ * Created by hyliu on 16/2/23.
+ */
+
+var ALY = require("aliyun-sdk")
+
+// 构建一个 Aliyun Client, 用于发起请求
+// 构建Aliyun Client时需要设置AccessKeyId和AccessKeySevcret
+// RAM是Global Service, API入口位于杭州, 这里使用RAM API的主地址
+var green = new ALY.GREEN({
+    accessKeyId: '你的accessKeyId',
+    secretAccessKey: '你的secretAccessKey',
+    endpoint: 'http://green.cn-hangzhou.aliyuncs.com',
+    apiVersion: '2016-11-24'
+});
+
+green.textKeywordFilter({
+        Text:'蒙汗药'
+    },
+    function(err, data){
+        if(err) {
+            console.log('error:', err);
+            return;
+        }
+        console.log('success:', JSON.stringify(data));
+    }
+);
