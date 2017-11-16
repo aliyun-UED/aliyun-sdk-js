c43c38d0d435bb3cae87c9eb73ccd32aeeabda8e
aleelock <aleelock@126.com>
2017-11-16 13:51:17 +0800

    list buckets支持marker

--- samples/oss/ListBucket.js
+++ samples/oss/ListBucket.js
@@ -4,7 +4,10 @@ var oss = require('./oss');
 // 5.2.1 GetService (ListBucket)
 // -------------------------------
 
-oss.listBuckets(function (err, data) {
+oss.listBuckets({
+	Marker: '',
+	MaxKeys: 100
+},function (err, data) {
   if (err) {
     console.log('error:', err);
     return;
