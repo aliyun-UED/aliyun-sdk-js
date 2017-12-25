ef2c9db752906459fd744190550cbc4b227c0f7e
daxingplay <daxingplay@gmail.com>
2017-12-25 21:13:29 +0800

    add new test case for sls 2015-06-01

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 //使用mocha运行: mocha index
 //如果没有mocha请先安装: npm i -g mocha
 
-require('./sls');
+require('./sls/2014-11-18');
+require('./sls/2015-06-01');
 require('./batchcompute/2015-06-30');
 require('./batchcompute/2015-11-11');
